2719a7c1e070dccc275ccf10341c2811aa53fef8
rahulraghava <34130990+rahulraghava@users.noreply.github.com>
2019-08-09 20:15:10 +0530

    Add files via upload

---
+++ application.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import mysql.connector as sql
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    c = sql.connect(host='172.16.0.2',user='rahul',password='rahul.281294',database='testDB')
+    a = c.cursor()
+    a.execute("SELECT * FROM Persons")
+    b = a.fetchone()
+    f = ''.join(map(str,b))
+    return f
+
+app.run()
